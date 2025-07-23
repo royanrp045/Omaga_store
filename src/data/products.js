77aa0ec4472978b231bc0fdfb48969db996f64c1
Royanrp045 <royanristuprayoga@gmail.com>
2025-07-24 03:01:37 +0700

    product 4

--- src/data/products.js
+++ src/data/products.js
@@ -1,20 +1,27 @@
 const products = [
   {
     id: 1,
+    name: "Los Angeles",
+    imageUrl: "/images/s4.png", // GANTI dengan URL gambar asli baju Anda
+    robloxLink: "https://www.roblox.com/catalog/118022102375454",
+    price: "R$ 5", // GANTI dengan link baju Roblox Anda
+  },
+  {
+    id: 2,
     name: "just do it",
     imageUrl: "/images/s3.png", // GANTI dengan URL gambar asli baju Anda
     robloxLink: "https://www.roblox.com/catalog/95260955056336",
     price: "R$ 5", // GANTI dengan link baju Roblox Anda
   },
   {
-    id: 2,
+    id: 3,
     name: "Langor Drevic",
     imageUrl: "/images/s2.png", // GANTI dengan URL gambar asli baju Anda
     robloxLink: "https://www.roblox.com/catalog/136611011027449",
     price: "R$ 5",
   },
   {
-    id: 3,
+    id: 4,
     name: "Omagaa",
     imageUrl: "/images/s1.png", // GANTI dengan URL gambar asli baju Anda
     robloxLink: "https://www.roblox.com/catalog/82083402559151",
